8339ab22b1253db99c7f0aaaee63b4eb69e43c11
Mathew Cook <cook6348@gmail.com>
2021-04-18 19:27:55 -0500

    Day 3 python

---
+++ Day3Python.py
@@ -0,0 +1,32 @@
+#Conditionals
+# print("Welcome to the rollercoaster")
+# height = int(input("What is your height in cm? "))
+
+# if height >= 120:
+#     print('You can ride the rollarcoaster!!')
+# else:
+#     print("Your not tall enough to ride the rollarcoaster")
+
+#Odd or even practice
+# numberToCheck = int(input("Please give me a number, and I'll tell you if its odd or even. "))
+
+# if numberToCheck % 2 == 0:
+#     print("This is an even number")
+# else:
+#     print("This is an odd number")
+
+#Nested Conditionals
+# weight = float(input('What is your weight?'))
+# height = float(input('What is your height?'))
+# result = round(weight / height ** 2)
+
+# if result < 18.5:
+#     print(f"Your BMI is {result}, you are underweight.")
+# elif result < 25:
+#     print(f"Your BMI is {result}, you have a normal weight.")
+# elif result < 30:
+#     print(f"Your BMI is {result}, you are slightly overweight.")
+# elif result < 35:
+#     print(f"Your BMI is {result}, you are obese.")
+# else:
+#     print(f"Your BMI is {result}, you are clinically obese.")
