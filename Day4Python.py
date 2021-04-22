e26181f25d232bf39d497486c095c0d68c41b925
Mathew Cook <cook6348@gmail.com>
2021-04-22 15:38:06 -0500

    Adding some error handeling

--- Day4Python.py
+++ Day4Python.py
@@ -85,23 +85,30 @@ choices = [rock, paper, scissors]
 player_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
 computer_choice = random.randint(0,2)
 
+if player_choice >= 3 or player_choice < 0:
+    print("INVALID NUMBERS YOU LOSE")
+else:
+    print(choices[player_choice])
+    print("Computer chose:")
+    print(choices[computer_choice])
 
-print(choices[int(player_choice)])
-print("Computer chose:")
-print(choices[computer_choice])
 
-if player_choice == 0 and computer_choice == 1:
-    print("You lose")
-elif player_choice == 1 and computer_choice == 0:
-    print("You win")
-elif player_choice == 1 and computer_choice == 2:
-    print("You lose")
-elif player_choice == 2 and computer_choice == 1:
-    print("You win")
-elif player_choice == 2 and computer_choice == 0:
-    print("You lose")
-elif player_choice == 0 and computer_choice == 2:
-    print("You lose")
+
+    if player_choice == 0 and computer_choice == 1:
+        print("You lose")
+    elif player_choice == 1 and computer_choice == 0:
+        print("You win")
+    elif player_choice == 1 and computer_choice == 2:
+        print("You lose")
+    elif player_choice == 2 and computer_choice == 1:
+        print("You win")
+    elif player_choice == 2 and computer_choice == 0:
+        print("You lose")
+    elif player_choice == 0 and computer_choice == 2:
+        print("You lose")
+    elif player_choice ==  computer_choice:
+        print("DRAW!!")
+
 
 
 
